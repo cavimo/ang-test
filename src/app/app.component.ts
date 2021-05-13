8a8b11b38a891009e6470c399ef087c39e95d27c
cavimo <carlos.vinya@gmail.com>
2021-05-13 20:38:42 +0200

    hola

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  nom = 'Carlos';
+  cognom = 'Viña';
+  curs_cicle = 'DAM2';
+  edat = 28;
+  email = 'prova@correu.net';
+  estat_treballador = 'actiu';
+  nomines = '123456';
+  arr = [10, 10, 30];
+  alumne = "Joan Perez";
+  cicle = "DAM";
+  curs = 2;
 }
